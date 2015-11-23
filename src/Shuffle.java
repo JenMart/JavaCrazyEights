ba870822604737645c47d25e2d345d147bbfcb14
jensinam <inertial.fey@gmail.com>
2015-11-22 23:32:56 -0600

    UpdateNEW 11/22

--- src/Shuffle.java
+++ src/Shuffle.java
@@ -6,12 +6,12 @@ import java.util.Random;
  */
 public class Shuffle extends CardDeck { //extends CardDeck
     static String card = deck.get(0).toString();
-
     public static String PickACard(){
         //used to hand out an individual card to player or PC when requested.
-       card = deck.get(0).toString();
+        card = deck.get(0).toString();
         discard.add(deck.get(0)); //adds card to discard ArrayList...
         deck.remove(0); //..before removing it from the main deck
         return card;
     }
+
 }
